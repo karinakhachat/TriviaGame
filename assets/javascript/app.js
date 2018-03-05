8c2864432bb8e828f7b0396d160a3c8f6dcbb51e
Karina Khachatryan <karinakhachat@Karinas-Air.fios-router.home>
2018-03-04 20:14:56 -0500

    timer working but nnot the append function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,100 +8,95 @@ var userselect = 0;
 
 
 var Questions = [{
-        question: "What is the only state in America that begins with the letter p?",
-        answer: ["Prague", "Pago Pago", "Philadlephia", "Pennsylvania"],
-        correctans: [3].answer
-    },
-    {
-        question: "What is the worlds largest island?",
-        answer: ["Fiji", "Greenland", "Madagascar", "Borneo"],
-        correctans: [2].answer
-    },
+    question: "What is the only state in America that begins with the letter p?",
+    answer: ["Prague", "Pago Pago", "Philadlephia", "Pennsylvania"],
+    correctans: [3].answer
+},
+{
+    question: "What is the worlds largest island?",
+    answer: ["Fiji", "Greenland", "Madagascar", "Borneo"],
+    correctans: [2].answer
+},
 
-    {
-        question: "What is the diameter of the Earth?",
-        answer: ["8,000 miles", "20,000 miles", "6,000 miles", "100,000 miles"],
-        correctans: [1].answer
-    },
-
-
-    {
-        question: "What is the worlds largest ocean?",
-        answer: ["Atlantic", "Arctic", "Pacific", "Indian"],
-        correctans: [3].answer
-    },
-
-    {
-        question: "What is the capital of Armenia",
-        answer: ["Tel-aviv", "Yerevan", "Moscow", "Mexico-city"],
-        correctans: [2].answer
-    },
-
-    {
-        question: "What country is Prague in?",
-        answer: ["Czech-Republic", "Russia", "Mexico", "India"],
-        correctans: [1].answer
+{
+    question: "What is the diameter of the Earth?",
+    answer: ["8,000 miles", "20,000 miles", "6,000 miles", "100,000 miles"],
+    correctans: [1].answer
+},
 
 
-    },
+{
+    question: "What is the worlds largest ocean?",
+    answer: ["Atlantic", "Arctic", "Pacific", "Indian"],
+    correctans: [3].answer
+},
 
-    {
-        question: "What color is a Welsh Poppy Flower?",
-        answer: ["Red", "Pink", "Orange", "Yellow"],
-        correctans: [4].answer
-    },
+{
+    question: "What is the capital of Armenia",
+    answer: ["Tel-aviv", "Yerevan", "Moscow", "Mexico-city"],
+    correctans: [2].answer
+},
 
-    {
-        question: "Name the three primary colours?",
-        answer: ["Yellow, Orange, Green", "Blue, Red, Yellow", "Brown, White, Red", "Yellow, Blue, Green"],
-        correctans: [2].answer
-    }
+{
+    question: "What country is Prague in?",
+    answer: ["Czech-Republic", "Russia", "Mexico", "India"],
+    correctans: [1].answer
+
+
+},
+
+{
+    question: "What color is a Welsh Poppy Flower?",
+    answer: ["Red", "Pink", "Orange", "Yellow"],
+    correctans: [4].answer
+},
+
+{
+    question: "Name the three primary colours?",
+    answer: ["Yellow, Orange, Green", "Blue, Red, Yellow", "Brown, White, Red", "Yellow, Blue, Green"],
+    correctans: [2].answer
+}
 
 ]
 console.log(Questions)
-    //works
+//works
 
 //wrapt this in the $(document).ready test
-$(document).ready(
+$(document).ready(function() {
 
-    function radiobutton() {
-    //test your values
-    console.log(Questions.answer.length);
-    for (Questions.keys(answer.length).forEach(function(keys){
-    Questions.append('<label><input type="radio" name="questions" value=""/></label>');
-    
-    
-    }
+    //function radiobutton() {
+        //test your values
+        //console.log(Questions.answer.length);
+        //for(Questions.keys(answer.length).forEach(function (keys) {
+            //Questions.append('<label><input type="radio" name="questions" value=""/></label>');
 
-    radiobutton();
+
+//      }
 
 
     function Start() {
 
-        //Use console.log like this to show which parts of your code is running.
-        console.log("start called");
+                //Use console.log like this to show which parts of your code is running.
+                console.log("start called");
 
-        $("#submit").click(function() {
-            $(".container").append(Questions);
-            // setTimeout(fivemin, 10000 * 5);
-            setTimeout(fivemin, 100 * 5); // short test
+                $("#submit").click(function () {
+                    $("#pleasework").append(Questions);
+                console.log(Questions);
+                    // setTimeout(fivemin, 10000 * 5);
+                    setTimeout(fivemin, 100 * 5); // short test
 
-            console.log("Timeout done")
+                    console.log("Timeout done")
 
-            function fivemin() {
-                $("#timer").append("<h1>You have five minutes<h1>")
+                    function fivemin() {
+                        $("#timer").append("<h1>You have five minutes<h1>")
 
+                    }
+
+                })
             }
-
-        })
-    }
     //call the function
-    $("#sumbit").on("click", Start());
-
-}
-
+Start();
 //call the function
 
 
-)
-//beginning
+        });
